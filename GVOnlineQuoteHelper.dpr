d62c2324f50e29970f6432d58fb58eba78fe0c77
GVOnline Helper <gvonline.helper@gmail.com>
2017-08-23 17:38:00 +0900

    Hot fix :: Set program single running.

--- GVOnlineQuoteHelper.dpr
+++ GVOnlineQuoteHelper.dpr
@@ -4,6 +4,7 @@ program GVOnlineQuoteHelper;
 
 uses
   Vcl.Forms,
+  Windows,
   main in 'main.pas' {FormQuote},
   gamestate in 'gamestate.pas',
   request in 'request.pas';
@@ -11,6 +12,10 @@ uses
 {$R *.res}
 
 begin
+  CreateMutex(nil, true, '547F8B98-116B-41FC-A858-1CB9DDC752BF');
+  if GetLastError = ERROR_ALREADY_EXISTS then
+    exit;
+
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TFormQuote, FormQuote);
